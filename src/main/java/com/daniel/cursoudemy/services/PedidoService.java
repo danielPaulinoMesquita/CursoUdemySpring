99aa708e73abc572ea9ed8cd2ce38a6fd95ce638
Daniel Paulino <daniel-paulino@hotmail.com>
2018-12-13 17:07:24 -0200

    1.0.1 Endpoint /pedidos/{id} disponibilizando

---
+++ src/main/java/com/daniel/cursoudemy/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.daniel.cursoudemy.services;
+
+import com.daniel.cursoudemy.domain.Pedido;
+import com.daniel.cursoudemy.exceptions.ObjectNotFoundException;
+import com.daniel.cursoudemy.repositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id) {
+        Optional<Pedido> pedido = repo.findById(id);
+        return pedido.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado! ID: " + id
+                + ", Tipo: " + Pedido.class.getName()));
+    }
+
+}
