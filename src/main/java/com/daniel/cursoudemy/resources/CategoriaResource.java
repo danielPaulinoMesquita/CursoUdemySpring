1bccb5b18116dbdcba920683785df86f072cdbca
Daniel Paulino <daniel-paulino@hotmail.com>
2018-12-15 14:21:26 -0200

    1.0.2 Inserindo uma nova Categoria com Post

--- src/main/java/com/daniel/cursoudemy/resources/CategoriaResource.java
+++ src/main/java/com/daniel/cursoudemy/resources/CategoriaResource.java
@@ -5,6 +5,9 @@ import com.daniel.cursoudemy.domain.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -17,8 +20,16 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
         Categoria obj = service.buscar(id);
-        //  return new ResponseEntity<>(obj, HttpStatus.OK); OUTRA FORMA DE RETORNA O OBJ EM JSON
+        //  return new ResponseEntity<>(obj, HttpStatus.OK); OUTRA FORMA DE RETORNAR O OBJ EM JSON
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
