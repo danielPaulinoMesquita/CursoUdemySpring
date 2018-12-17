e106105a7c16eaa2339dd18f5db8677a942629ed
Daniel Paulino <daniel-paulino@hotmail.com>
2018-12-17 20:25:52 -0200

    1.0.2 Correções: update categoriaService e construtor de Pagamento

--- src/main/java/com/daniel/cursoudemy/domain/Pagamento.java
+++ src/main/java/com/daniel/cursoudemy/domain/Pagamento.java
@@ -19,17 +19,17 @@ public abstract class Pagamento implements Serializable {
     /*@JsonBackReference tem o mesmo objetivo do managedReference, com funcionalidade de tornar a classe não serializavel*/
     @JsonIgnore
     @OneToOne
-    @JoinColumn(name="pedido_id")
+    @JoinColumn(name = "pedido_id")
     @MapsId
     private Pedido pedido;
 
-    public Pagamento(){
+    public Pagamento() {
 
     }
 
     public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
         this.id = id;
-        this.estado = estado.getCod();
+        this.estado = (estado == null) ? null : estado.getCod();
         this.pedido = pedido;
     }
 
