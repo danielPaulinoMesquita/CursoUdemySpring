6a04a96e202646b4287d08d6fce7fc9f43777f63
Daniel Paulino <daniel-paulino@hotmail.com>
2018-12-20 21:35:30 -0200

    1.0.2 Criando profile de teste

---
+++ src/main/java/com/daniel/cursoudemy/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.daniel.cursoudemy.config;
+
+import com.daniel.cursoudemy.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
