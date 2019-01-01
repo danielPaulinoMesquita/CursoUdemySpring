3da7bf521bded5b2c2e5962465c0eef475279514
Daniel Paulino <daniel-paulino@hotmail.com>
2018-12-31 23:27:19 -0200

    1.0.2 capitulo email, implementando EMAIL HTML

--- src/main/java/com/daniel/cursoudemy/services/EmailService.java
+++ src/main/java/com/daniel/cursoudemy/services/EmailService.java
@@ -2,10 +2,15 @@ package com.daniel.cursoudemy.services;
 
 import com.daniel.cursoudemy.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
+import javax.mail.internet.MimeMessage;
 
 public interface EmailService {
 
     void sendOrderConfimationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/daniel/cursoudemy/services/SmtpEmailService.java
+++ src/main/java/com/daniel/cursoudemy/services/SmtpEmailService.java
@@ -5,11 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService{
 
     @Autowired
     private MailSender mailSender;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
 
     public static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
@@ -19,6 +25,13 @@ public class SmtpEmailService extends AbstractEmailService{
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Enviando o email...");
         mailSender.send(msg);
+        LOG.info("Email Enviado");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando o email...");
+        javaMailSender.send(msg);
         LOG.info("Email Enviado");
     }
 }
