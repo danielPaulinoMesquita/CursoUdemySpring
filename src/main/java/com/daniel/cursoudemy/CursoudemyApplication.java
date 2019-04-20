eff5eeaf82025bb734443aa4adc13f24e229aa95
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2019-04-19 21:40:00 -0300

    1.0.3 AWS, Salvando primeiro arquivo no S3

--- src/main/java/com/daniel/cursoudemy/CursoudemyApplication.java
+++ src/main/java/com/daniel/cursoudemy/CursoudemyApplication.java
@@ -1,6 +1,8 @@
 package com.daniel.cursoudemy;
 
 
+import com.daniel.cursoudemy.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoudemyApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoudemyApplication.class, args);
@@ -16,7 +20,7 @@ public class CursoudemyApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-
+        s3Service.uploadFile("//home//daniel//Imagens//finalfantasyx.jpeg");
 
     }
 }
