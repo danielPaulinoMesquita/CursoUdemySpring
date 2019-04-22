f28c03763d3d81508c8b44661c8b64b69de2e29d
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2019-04-22 15:48:57 -0300

    1.0.3 AWS, Ajustando o tamanho da imagem

--- src/main/java/com/daniel/cursoudemy/services/ImageService.java
+++ src/main/java/com/daniel/cursoudemy/services/ImageService.java
@@ -2,6 +2,7 @@ package com.daniel.cursoudemy.services;
 
 import com.daniel.cursoudemy.exceptions.FileException;
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -42,13 +43,24 @@ public class ImageService {
         return jpgImage;
     }
 
-    public InputStream getInputStream(BufferedImage img, String extension){
+    public InputStream getInputStream(BufferedImage img, String extension) {
         try {
-            ByteArrayOutputStream os= new ByteArrayOutputStream();
-            ImageIO.write(img,extension,os);
-            return  new ByteArrayInputStream(os.toByteArray());
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(img, extension, os);
+            return new ByteArrayInputStream(os.toByteArray());
         } catch (IOException e) {
             throw new FileException("Erro ao ler arquivo");
         }
+    }
+
+    public BufferedImage cropSquare(BufferedImage sourceImg) {
+        int min = (sourceImg.getHeight() <= sourceImg.getWidth() ? sourceImg.getHeight() : sourceImg.getWidth());
+        return Scalr.crop(sourceImg,
+                (sourceImg.getWidth() / 2) - (min / 2),
+                (sourceImg.getHeight() / 2) - (min / 2), min, min);
+    }
+
+    public BufferedImage resize(BufferedImage sourceImg, int size) {
+        return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
     }
 }
